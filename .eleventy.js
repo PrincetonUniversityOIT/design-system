15a0e4e3816f4942875afa84cb488ebf6eaaa883
mzarillo <mzarillo@princeton.edu>
2021-01-14 08:51:51 -0500

    UXODS-173 Initial Documentation Framework Design

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,38 @@
+const fs = require('fs');
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+const configObj = {
+    dir: {
+        input: "docs"
+    }
+};
+
+// function escapeHTML(str) {
+//     return str.replace(/[&<>'"]/g,
+//         tag => ({
+//             '&': '&amp;',
+//             '<': '&lt;',
+//             '>': '&gt;',
+//             "'": '&#39;',
+//             '"': '&quot;'
+//         }[tag]));
+// }
+
+function getSourceCode(config, path) {
+    // return process.cwd() + '/' + config.dir.input + '/' + path + '.code';
+    var fileToProcess = process.cwd() + '/' + config.dir.input + '/' + path + '.code';
+    fileToProcess = fileToProcess.replace('/./', '/');
+    fileToProcess = fileToProcess.replace('/', '\\');
+    return fs.readFileSync(fileToProcess, 'utf8');
+}
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.setUseGitIgnore(false);
+
     eleventyConfig.addWatchTarget("./dist/");
+    eleventyConfig.addWatchTarget("./docs/**/*.code");
+
     eleventyConfig.addPassthroughCopy({ "dist/styles.css": "styles.css" });
     eleventyConfig.addPassthroughCopy({ "dist/fonts.css": "fonts.css" });
     eleventyConfig.addPassthroughCopy({ "dist/fonts": "fonts" });
@@ -9,10 +40,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "dist/img": "img" });
     eleventyConfig.addPassthroughCopy({ "dist/logos": "logos" });
     eleventyConfig.addPassthroughCopy({ "dist/design_system.js": "design_system.js" });
+    eleventyConfig.addPassthroughCopy({ "docs/css/prism-atom-dark.css": "prism-atom-dark.css" });
 
-    return {
-        dir: {
-            input: "docs"
-        }
-    };
+    eleventyConfig.addNunjucksShortcode("src", function(path) { return getSourceCode(configObj, path); });
+
+    eleventyConfig.addPlugin(syntaxHighlight, {
+        lineSeparator: "\n",
+    });
+
+    return configObj;
 };
