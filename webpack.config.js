20b2da50b7219bc4a52d0d70efe9e95168386353
mzarillo <mzarillo@princeton.edu>
2021-04-21 16:20:06 -0400

    UXODS-21 proof-of-concept for CSS variable support in theming

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         jazz_serif: './src/scss/jazz_serif.scss',
         jazz_sans: './src/scss/jazz_sans.scss',
         jazz_fonts: './src/scss/jazz_fonts.scss',
+        jazz_custom: './src/scss/jazz_custom.scss',
         jazz_behavior: './src/jazz_behavior.ts'
     },
 
