81cb91e136c20005b8141154b560a72a0acdbcdb
Rosemarie Sarigumba <rs16@princeton.edu>
2021-04-28 15:47:54 -0400

    Remove logic to disable pager and only trigger pager behavior when the total pages and current page is set in the jazz-pager

--- src/js/jazz_behavior.ts
+++ src/js/jazz_behavior.ts
@@ -5,43 +5,29 @@ import {MenuToggleBehavior} from "./components/menu-toggle/menu-toggle";
 import {PagerBehavior} from "./components/pager/pager";
 import {TabsBehavior} from "./components/tabs/tabs";
 
-export class BehaviorOptions {
-    public disablePager?: boolean
-}
-
 export class PrincetonDesignSystem {
 
     // options allow consumers to disable behavior for components if they want to handle the behavior on their own
-    public enableDesignSystem(options?: BehaviorOptions) {
-        this.enable(options);
-        this.disable(options);
+    public enableDesignSystem() {
+        this.enable();
+        this.disable();
     }
 
-    public enable(options?: BehaviorOptions) {
+    public enable() {
         new AccordionBehavior().enable();
         new ModalDialogBehavior().enable();
         new HeaderBehavior().enable();
         new MenuToggleBehavior().enable();
-
-        // if pager is not disabled
-        if (!options || (options && !options.disablePager)) {
-            new PagerBehavior().enable();
-        }
-
+        new PagerBehavior().enable();
         new TabsBehavior().enable();
     }
 
-    public disable(options?: BehaviorOptions) {
+    public disable() {
         new AccordionBehavior().disable();
         new ModalDialogBehavior().disable();
         new HeaderBehavior().disable();
         new MenuToggleBehavior().disable();
-
-        // if pager is not disabled
-        if (!options || (options && !options.disablePager)) {
-            new PagerBehavior().disable();
-        }
-
+        new PagerBehavior().disable();
         new TabsBehavior().disable();
     }
 }
