004fdfdaa93d3be74ab1e1d156c17133647c3358
mzarillo <mzarillo@princeton.edu>
2021-05-10 09:53:38 -0400

    compact header pattern

---
+++ src/js/components/header/utility-header.ts
@@ -0,0 +1,59 @@
+import {ARIA_EXPANDED, Behavior} from '../../base/delegreater';
+import { prefix as PREFIX } from '../../config';
+import {Listener} from "../../base/decorator-functions";
+
+// Toggle Button Selector
+const NAV_TOGGLE_SELECTOR = `.${PREFIX}-utility-header__nav-toggle`;
+
+export class UtilityHeaderBehavior extends Behavior {
+
+    constructor() {
+        super();
+    }
+
+    currentRoot;
+
+    init(root: ParentNode) {
+
+        this.currentRoot = root;
+    }
+
+    @Listener({
+        event: 'click',
+        selector: NAV_TOGGLE_SELECTOR
+    })
+    onAction(event: Event) {
+
+        const button = <HTMLElement>event.target;
+
+        // This will handle when the click event happens for the icons within the button element
+        const targetButton = button.closest('button');
+        const expanded = targetButton.hasAttribute(ARIA_EXPANDED) && targetButton.getAttribute(ARIA_EXPANDED) === 'true';
+        const sectionEl = targetButton.closest('.jazz-utility-header__nav');
+        const iconEl = targetButton.querySelector('.jazz-icon');
+        const spanEl = targetButton.querySelector('span');
+        if (sectionEl) {
+            if (expanded) {
+                targetButton.setAttribute(ARIA_EXPANDED, 'false');
+                sectionEl.classList.remove('jazz-expanded');
+                iconEl.classList.remove('jazz-icon-close');
+                iconEl.classList.add('jazz-icon-menu');
+                spanEl.innerText = 'Open Navigation Menu';
+            } else {
+                targetButton.setAttribute(ARIA_EXPANDED, 'true');
+                sectionEl.classList.add('jazz-expanded');
+                iconEl.classList.remove('jazz-icon-menu');
+                iconEl.classList.add('jazz-icon-close');
+                spanEl.innerText = 'Close Navigation Menu';
+            }
+        }
+    }
+
+    // This retrieves the appropriate button depending on the selector passed in
+    getButtonForSelector(btnSelector, button, mainEl) {
+        if (!button.matches(btnSelector)) {
+            button = mainEl.querySelector(btnSelector);
+        }
+        return button;
+    }
+}
--- src/js/jazz_behavior.ts
+++ src/js/jazz_behavior.ts
@@ -1,25 +1,31 @@
 import {AccordionBehavior} from './components/accordion/accordion';
 import {ModalDialogBehavior} from "./components/modal-dialog/modal-dialog";
 import {HeaderBehavior} from "./components/header/header";
+import {UtilityHeaderBehavior} from "./components/header/utility-header";
 import {MenuToggleBehavior} from "./components/menu-toggle/menu-toggle";
 import {PagerBehavior} from "./components/pager/pager";
 import {TabsBehavior} from "./components/tabs/tabs";
 
 export class PrincetonDesignSystem {
 
+    public scrolling = false;
+    public scrollInterval;
+
     // options allow consumers to disable behavior for components if they want to handle the behavior on their own
     public enableDesignSystem() {
-        this.enable();
         this.disable();
+        this.enable();
     }
 
     public enable() {
+        this.startTrackingScroll();
         new AccordionBehavior().enable();
         new ModalDialogBehavior().enable();
         new HeaderBehavior().enable();
         new MenuToggleBehavior().enable();
         new PagerBehavior().enable();
         new TabsBehavior().enable();
+        new UtilityHeaderBehavior().enable();
     }
 
     public disable() {
@@ -29,5 +35,44 @@ export class PrincetonDesignSystem {
         new MenuToggleBehavior().disable();
         new PagerBehavior().disable();
         new TabsBehavior().disable();
+        new UtilityHeaderBehavior().disable();
+        this.stopTrackingScroll();
+    }
+
+    public startTrackingScroll() {
+        window.addEventListener('scroll', () => {
+            this.scrolling = true;
+        });
+
+        this.scrollInterval = setInterval(this.updateScrollCoords, 250 );
+    }
+
+    public updateScrollCoords = () => {
+        if (this.scrolling) {
+            this.scrolling = false;
+            this.scrollEvent();
+        }
+    }
+
+    public scrollEvent() {
+        var htmlEl = document.getElementsByTagName("html").item(0);
+        var scrollX = window.scrollX;
+        var scrollY = window.scrollY;
+        if (scrollX === 0) {
+            htmlEl.removeAttribute("data-jazz-scrollX");
+        } else {
+            htmlEl.setAttribute("data-jazz-scrollX", window.scrollX.toString());
+        }
+        if (scrollY === 0) {
+            htmlEl.removeAttribute("data-jazz-scrollY");
+        } else {
+            htmlEl.setAttribute("data-jazz-scrollY", window.scrollY.toString());
+        }
+    }
+
+    public stopTrackingScroll() {
+        clearInterval(this.scrollInterval);
+        window.removeEventListener("scroll", this.scrollEvent);
+        this.scrolling = false;
     }
 }
